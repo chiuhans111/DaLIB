4c42458367a2b07c76ac4e01daca23bc8d3c562f
Hans Chiu <chiuhans111@gmail.com>
2018-04-25 21:25:53 +0800

    try new approch

--- GoldBrain/server/data/base.js
+++ GoldBrain/server/data/base.js
@@ -11,29 +11,34 @@ var config = require('../config/config');
 // change db url here
 var url = config.db;
 
+
+
 function connect(url) {
-    console.log('connecting to db...')
-    return mongoose.connect(url).then(_ => {
-        console.log('connected');
-        return db.connect(url);
-    }).then(db => {
-        mongoose.db = db;
-        return mongoose;
-    }).catch(retry);
+    var promise_return = function () { }
+    var promise = new Promise(done => {
+        promise_return = done;
+    })
+
+    function tryConnect() {
+        try {
+            mongoose.connect(url);
+            mongoose.db = db.connect(url);
+            promise_return(done);
+        } catch (e) {
+            console.error("connecting DB error");
+            console.error("please check is DB opened");
+            console.error("try again in 5 second");
+            setTimeout(function () {
+                console.log('after five seconds...')
+                tryConnect();
+            }, 5000);
+        }
+    }
+    
+    tryConnect();
+    return promise;
 }
 
-function retry(error) {
-    mongoose.disconnect();
-    console.error("connecting DB error");
-    console.error("please check is DB opened");
-    console.error("try again in 5 second");
-    return new Promise(done => {
-        setTimeout(function () {
-            var result = connect();
-            done(result);
-        }, 5000);
-    })
-}
 
 /**@type {Promise.<_MG>} */
 module.exports = connect(url);
